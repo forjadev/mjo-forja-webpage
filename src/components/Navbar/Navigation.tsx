9fe21e7b86a551ce7f3307a7c644a1a95c81424e
unknown <61201131+kevinCubas@users.noreply.github.com>
2023-07-27 02:24:42 -0300

    feat(component): create Navbar component base structure

---
+++ src/components/Navbar/Navigation.tsx
@@ -0,0 +1,18 @@
+const Navigation = () => {
+  {/*to do: add tailwind classes*/}
+  {/*to do: create variants*/}
+  return (
+    <nav className="">
+      <ul className="">
+        {/* to do: separate <li/> components and pass text content as props  */}
+        {/* to do: add anchor tags from next */}
+        <li className="">Início</li>
+        <li className="">Sobre</li>
+        <li className="">Conteúdos</li>
+        <li className="">Seja <span className="">PRO</span></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation
