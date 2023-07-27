eb25cfcfb5fc5eb76174b384cafe57ddf6c15f67
Arthur Andrade <git@arthur404.dev>
2023-07-27 19:36:48 -0300

    feat: create button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
+import { Button } from "@/components"
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <Button />
+    </main>
   )
 }
