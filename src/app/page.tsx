636e531a086ab386895288327170a6d4534f4d25
Arthur Andrade <git@arthur404.dev>
2023-07-27 19:36:48 -0300

    feat: create button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import { DM_Sans, Space_Grotesk } from 'next/font/google'
  
 export default function Home() {
   return (
-      <main className={`flex min-h-screen text-metal flex-col
-                        items-center justify-between p-24`}>
-      </main>
-      )
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <Button />
+    </main>
+  )
 }
