5163c845d8a0d491ae7b00b4a1e1b2a1efc9463a
GisellyRock <gisellydsr@gmail.com>
2023-07-31 17:29:28 -0300

    feat: button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,7 +1,11 @@
 import React from "react"
 
 const Button = () => {
-  return <div>Button</div>
+  return <button
+  className="
+  text-2xl font-bold p-1 rounded-md bg-gradient-to-br from-[#5480C5] to-[#5ACD84]">
+    <div className="m-full h-full bg-black py-4 px-8 rounded">Vamos nessa!</div>
+  </button>
 }
 
 export default Button
