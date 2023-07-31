27b88d2b259497d7805f1aeccda711221c5038f1
Junio Santos <git@junio.dev>
2023-07-31 17:26:33 -0300

    feat: add design system's variables and dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,29 @@
 "use client"
 import { Button } from "@/components"
 
+import { DM_Sans, Space_Grotesk } from 'next/font/google'
+ 
+const space_grotesk = Space_Grotesk({
+  subsets: ['latin'],
+  variable: '--font-space-grotesk',
+  weight: "700",
+  style: "normal",
+})
+ 
+const dm_sans = DM_Sans({
+  subsets: ['latin'],
+  variable: '--font-dm-sans',
+  weight: ["400", "500", "700"],
+  style: ["normal", "italic"],
+})
+
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24 bg-[#dce3f1] dark:bg-[#191B21]'>
-      <Button size='sm'>Botão 1</Button>
-      <Button size='md' variant='info'>
-        Botão 2
-      </Button>
-      <Button size='lg'>Botão 3</Button>
-    </main>
-  )
+      <main 
+      className={
+        `${space_grotesk.variable} ${dm_sans.variable}
+        flex min-h-screen text-metal flex-col items-center justify-between p-24`
+      }>
+      </main>
+      )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,52 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'dev': {
+          'primary': '#3ADBC8',
+          'secondary': '#1E82BA',
+        },
+        'pro': {
+          'primary': '#FBB361',
+          'secondary': '#CB861E',
+        },
+        'surface-primary': {
+          dark: '#191B21',
+          light: '#EFF2F8',
+        },
+        'surface-secondary': {
+          dark: '#1F2025',
+          light: '#DCE3F1',
+        },
+        'on-primary': {
+          dark: '#D9E0EE',
+          light: '#161320',
+        },
+        'on-secondary': {
+          dark: '#A09EB2',
+          light: '#575268',
+        },
+        'on-neutral': {
+          dark: '#202036',
+          light: '#EFF2F8',
+        },
+        'moss': {
+          dark: '#3B6670',
+          light: '#C3BAC6',
+        },
+        'metal': {
+          dark: '#3F3F46',
+          light: '#D4D4D8',
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        'display': ['var(--font-space-grotesk)', 'Helvetica', 'Arial', 'Open Sans'],
+        'body': ['var(--font-dm-sans)', 'Helvetica', 'Arial', 'Open Sans'],
       },
     },
   },
