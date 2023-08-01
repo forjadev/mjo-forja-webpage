df8da8249870c28aaf4fa594378c976d0f51c51a
Arthur Andrade <git@arthur404.dev>
2023-08-01 18:50:47 -0300

    feat: add structure of Button component for forwardRef

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+"use client"
 import { Button } from "@/components"
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <Button />
+      <Button>Vamos Nessa!</Button>
+      <Button>Outro Texto</Button>
+      <Button>Vamos Pra Lá!</Button>
     </main>
   )
 }
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,11 +1,24 @@
 import React from "react"
 
-const Button = () => {
-  return <button
-  className="
-  text-2xl font-bold p-1 rounded-md bg-gradient-to-br from-[#5480C5] to-[#5ACD84]">
-    <div className="m-full h-full bg-black py-4 px-8 rounded">Vamos nessa!</div>
-  </button>
+export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  primary?: boolean
 }
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ primary, children, ...props }: ButtonProps, ref) => {
+    return (
+      <button
+        ref={ref}
+        {...props}
+        className='
+  text-2xl font-bold px-8 py-4 rounded-md border-2 border-[#3ADBC8] hover:bg-[#3ADBC8] hover:text-[#202036] transition-all ease-out cursor-pointer'
+        onClick={() => alert("Vamos Nessa!")}
+      >
+        {children}
+      </button>
+    )
+  },
+)
+Button.displayName = "Button"
 
 export default Button
