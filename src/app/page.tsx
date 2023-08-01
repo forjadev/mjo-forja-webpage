7e8e4bfaeb1b7d14c1a5b0f878130c6e774dd708
Arthur Andrade <git@arthur404.dev>
2023-08-01 19:40:15 -0300

    feat: add darkmode enable by class and add styles to variants fitting color theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,12 @@ import { DM_Sans, Space_Grotesk } from 'next/font/google'
  
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <Button />
+    <main className='flex min-h-screen flex-col items-center justify-between p-24 bg-[#dce3f1] dark:bg-[#191B21]'>
+      <Button size='sm'>Botão 1</Button>
+      <Button size='md' variant='info'>
+        Botão 2
+      </Button>
+      <Button size='lg'>Botão 3</Button>
     </main>
   )
 }
