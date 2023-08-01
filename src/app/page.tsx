81fe0a29abcaf9e1928e2ce899c1b9ff6d5630f8
Samuel Pires <samuel.ferpires@gmail.com>
2023-08-01 19:31:40 -0300

    feat: Adding Social Media component to storybook | Fixing storybook configuration with tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import SocialMedia from '@/components/SocialMedia'
-import Image from 'next/image'
+import SocialMedia from "@/components/SocialMedia";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className='flex flex-col items-center justify-between min-h-screen p-24'>
-     <SocialMedia/>
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      <SocialMedia />
     </main>
-  )
+  );
 }
