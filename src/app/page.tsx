e91fef212bb474d427ff423bb012b1f0310d5bc4
Arthur Andrade <git@arthur404.dev>
2023-08-01 20:53:29 -0300

    feat: add globals for backgrounds and text enabling dark and lightmode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,37 @@
 "use client"
 import { Button } from "@/components"
 
-import { DM_Sans, Space_Grotesk } from 'next/font/google'
- 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24 bg-[#dce3f1] dark:bg-[#191B21]'>
-      <Button size='sm'>Botão 1</Button>
-      <Button size='md' variant='info'>
-        Botão 2
-      </Button>
-      <Button size='lg'>Botão 3</Button>
+    <main className='flex h-screen flex-col items-center justify-between p-24 bg-surface-primary'>
+      <div
+        className='flex flex-col items-center justify-center gap-36 w-3/4 h-2/3
+      bg-surface-secondary
+      rounded-xl ring-1 ring-metal-dark'
+      >
+        <div className='flex gap-4'>
+          <Button size='sm' variant='callers'>
+            Botão Caller
+          </Button>
+          <Button size='md' variant='callers'>
+            Botão Caller
+          </Button>
+          <Button size='lg' variant='callers'>
+            Botão Caller
+          </Button>
+        </div>
+        <div className='flex gap-4'>
+          <Button size='sm' variant='info'>
+            Botão Info
+          </Button>
+          <Button size='md' variant='info'>
+            Botão Info
+          </Button>
+          <Button size='lg' variant='info'>
+            Botão Info
+          </Button>
+        </div>
+      </div>
     </main>
   )
 }
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,13 +2,13 @@ import React from "react"
 import { twMerge } from "tailwind-merge"
 
 const base = {
-  style: `inline-flex items-center justify-center font-bold rounded-md transition-all ease-out cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none`,
+  style: `font-display inline-flex items-center justify-center font-bold rounded-md transition-all ease-out cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none`,
 }
 
 const variants = {
   style: {
-    callers: `${base.style} text-[#202036] border-2 border-[#1E82BA] hover:bg-[#1E82BA] hover:text-[#202036] hover:text-[#D9E0EE] focus:ring-[#1E82BA] dark:text-[#D9E0EE] dark:border-[#3ADBC8] dark:hover:bg-[#3ADBC8] dark:hover:text-[#202036] dark:focus:ring-[#3ADBC8]`,
-    info: `${base.style} text-[#202036] bg-[#FBB361] hover:bg-[#FF9F2F] focus:ring-[#202036]`,
+    callers: `${base.style} text-on-primary border-2 border-dev-secondary hover:bg-dev-secondary hover:text-on-neutral focus:ring-dev-secondary dark:border-dev-primary dark:hover:bg-dev-primary dark:focus:ring-dev-primary`,
+    info: `${base.style} text-on-neutral-dark bg-pro-primary hover:bg-pro-secondary focus:ring-on-neutral-dark`,
   },
   size: {
     sm: `px-4 py-2 text-sm gap-2`,
