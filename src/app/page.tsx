d8fa2118eb72f34a2b34e3f63ce9666657a5a5b6
Arthur Andrade <git@arthur404.dev>
2023-08-01 19:40:15 -0300

    feat: add darkmode enable by class and add styles to variants fitting color theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import { Button } from "@/components"
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <Button>Vamos Nessa!</Button>
-      <Button variant='info'>Outro Texto</Button>
-      <Button>Vamos Pra Lá!</Button>
+    <main className='flex min-h-screen flex-col items-center justify-between p-24 bg-[#dce3f1] dark:bg-[#191B21]'>
+      <Button size='sm'>Botão 1</Button>
+      <Button size='md' variant='info'>
+        Botão 2
+      </Button>
+      <Button size='lg'>Botão 3</Button>
     </main>
   )
 }
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,28 +1,44 @@
 import React from "react"
 
 const base = {
-  style: `text-2xl font-bold px-8 py-4 rounded-md transition-all ease-out cursor-pointer`,
+  style: `inline-flex items-center justify-center font-bold rounded-md transition-all ease-out cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none`,
 }
 
 const variants = {
   style: {
-    callers: `${base.style} border-2 border-[#3ADBC8] hover:bg-[#3ADBC8] hover:text-[#202036]`,
-    info: `${base.style} text-[#202036] bg-[#FBB361] hover:bg-[#FF9F2F]`,
+    callers: `${base.style} text-[#202036] border-2 border-[#1E82BA] hover:bg-[#1E82BA] hover:text-[#202036] hover:text-[#D9E0EE] focus:ring-[#1E82BA] dark:text-[#D9E0EE] dark:border-[#3ADBC8] dark:hover:bg-[#3ADBC8] dark:hover:text-[#202036] dark:focus:ring-[#3ADBC8]`,
+    info: `${base.style} text-[#202036] bg-[#FBB361] hover:bg-[#FF9F2F] focus:ring-[#202036]`,
+  },
+  size: {
+    sm: `px-4 py-2 text-sm`,
+    md: `px-6 py-3 text-lg`,
+    lg: `px-8 py-4 text-2xl`,
   },
 }
 
 export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   variant?: keyof typeof variants.style
+  size?: keyof typeof variants.size
+  handler?: () => void
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ variant = "callers", children, ...props }: ButtonProps, ref) => {
+  (
+    {
+      variant = "callers",
+      size = "lg",
+      handler,
+      children,
+      ...props
+    }: ButtonProps,
+    ref,
+  ) => {
     return (
       <button
         ref={ref}
         {...props}
-        className={variants.style[variant]}
-        onClick={() => alert("Vamos Nessa!")}
+        className={`${variants.style[variant]} ${variants.size[size]}`}
+        onClick={handler}
       >
         {children}
       </button>
