4e7fd876a163365370ac903e7ec4aaaa220a1549
Junio Santos <git@junio.dev>
2023-08-01 22:45:38 +0000

    chore[style]: indent class from App page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,9 @@
 import Image from 'next/image'
-
-import { DM_Sans, Space_Grotesk } from 'next/font/google'
  
-const space_grotesk = Space_Grotesk({
-  subsets: ['latin'],
-  variable: '--font-space-grotesk',
-  weight: "700",
-  style: "normal",
-})
- 
-const dm_sans = DM_Sans({
-  subsets: ['latin'],
-  variable: '--font-dm-sans',
-  weight: ["400", "500", "700"],
-  style: ["normal", "italic"],
-})
-
 export default function Home() {
   return (
-      <main 
-      className={
-        `${space_grotesk.variable} ${dm_sans.variable}
-        flex min-h-screen text-metal flex-col items-center justify-between p-24`
-      }>
+      <main className={`flex min-h-screen text-metal flex-col
+                        items-center justify-between p-24`}>
       </main>
       )
 }
