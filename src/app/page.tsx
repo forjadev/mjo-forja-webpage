94b27cc7a28ba673e251c71a0661cf8c325b34b3
victor brambilla <victor-brambilla@hotmail.com>
2023-08-01 19:58:03 -0400

    feat: add CommunityCard to storybook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,19 +11,3 @@ export default function Home() {
     </div>
   )
 }
-
-// "use client"
-
-// import {} from "@/components"
-
-// export function Home1() {
-//   return (
-//     <main className="flex h-screen flex-col items-center justify-between p-24 bg-surface-primary">
-//       <div
-//         className="flex flex-col items-center justify-center gap-36 w-3/4 h-2/3
-//       bg-surface-secondary
-//       rounded-xl ring-1 ring-surface-metal-dark"
-//       ></div>
-//     </main>
-//   )
-// }
