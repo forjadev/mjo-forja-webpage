02b23f5a2f4003af5c9df6e3f7dd8d9e459f723b
Sdney Lima <lima.rosimao@gmail.com>
2023-08-01 13:07:17 -0300

    feat: add icons social media component

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -1,14 +1,13 @@
-import {FaDiscord, FaGithub, FaLinkedin} from 'react-icons/fa'
-import {AiOutlineYoutube} from 'react-icons/ai'
+import { FaDiscord, FaGithub, FaLinkedin } from "react-icons/fa";
+import { AiOutlineYoutube } from "react-icons/ai";
 
 export default function SocialMedia() {
-    return(
-        <div className="gap-4 flex">
-            <FaDiscord size={24}/>
-            <FaGithub size={24}/>
-            <AiOutlineYoutube size={24}/>
-            <FaLinkedin size={24}/>
-        </div>
-    )
-
+  return (
+    <div className="gap-4 flex">
+      <FaDiscord size={24} />
+      <FaGithub size={24} />
+      <AiOutlineYoutube size={24} />
+      <FaLinkedin size={24} />
+    </div>
+  );
 }
