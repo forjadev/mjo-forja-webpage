5edb0800266dfe02c08eceae55a8fa027516725a
Samuel Pires <samuel.ferpires@gmail.com>
2023-08-08 19:50:53 -0300

    FEAT: Refactoring icons to use theme colors

--- src/components/SocialMedia/index.tsx
+++ src/components/SocialMedia/index.tsx
@@ -4,10 +4,22 @@ import { AiOutlineYoutube } from "react-icons/ai";
 export default function SocialMedia() {
   return (
     <div className="gap-4 flex">
-      <FaDiscord size={24} />
-      <FaGithub size={24} />
-      <AiOutlineYoutube size={24} />
-      <FaLinkedin size={24} />
+      <FaDiscord
+        size={24}
+        className="text-surface-secondary-dark dark:text-surface-secondary-light"
+      />
+      <FaGithub
+        size={24}
+        className="text-surface-secondary-dark dark:text-surface-secondary-light"
+      />
+      <AiOutlineYoutube
+        size={24}
+        className="text-surface-secondary-dark dark:text-surface-secondary-light"
+      />
+      <FaLinkedin
+        size={24}
+        className="text-surface-secondary-dark dark:text-surface-secondary-light"
+      />
     </div>
   );
 }
