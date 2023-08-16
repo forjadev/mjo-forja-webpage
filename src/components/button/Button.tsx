21232ab04a9f48ae0082a1803a0d2b57aa61ff1e
Arthur Andrade <git@arthur404.dev>
2023-08-16 20:40:37 -0300

    feat: add outline prop

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -4,7 +4,7 @@ import { cva, VariantProps } from "class-variance-authority"
 import React from "react"
 
 const buttonVariants = cva(
-  "font-display inline-flex items-center justify-center font-bold rounded-md transition-all ease-out cursor-pointer focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none transform active:scale-95 transition-transform",
+  "font-display inline-flex items-center justify-center font-bold rounded-md transition-all ease-out cursor-pointer focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none transform active:scale-95",
   {
     variants: {
       variant: {
@@ -14,20 +14,53 @@ const buttonVariants = cva(
           "text-on-neutral-dark bg-on-neutral-light hover:bg-surface-metal-light focus:ring-on-neutral-light dark:text-on-neutral-light dark:bg-surface-metal-dark dark:hover:bg-on-primary-light dark:focus:ring-surface-metal-dark",
         ghost:
           "text-on-neutral-dark bg-transparent hover:bg-surface-primary-dark hover:bg-opacity-10 focus:ring-surface-primary-dark/10 dark:text-on-neutral-light dark:hover:bg-surface-primary-light dark:hover:bg-opacity-10 dark:focus:ring-surface-primary-light/10",
-        primary: "text-on-primary-light bg-palette-cyan-300 hover:bg-palette-cyan-400 focus:ring-palette-cyan-300",
-        secondary: "text-on-neutral-dark bg-palette-purple-400 hover:bg-palette-purple-500 focus:ring-palette-purple-400 dark:text-on-neutral-light dark:bg-palette-purple-500 dark:hover:bg-palette-purple-600 dark:focus:ring-palette-purple-500",
-        accent: "text-on-neutral-dark bg-palette-yellow-400 hover:bg-palette-yellow-500 focus:ring-palette-yellow-400 dark:hover:text-on-neutral-light dark:bg-palette-yellow-500 dark:hover:bg-palette-yellow-600 dark:focus:ring-palette-yellow-500",
+        primary:
+          "text-on-primary-light bg-palette-cyan-300 hover:bg-palette-cyan-400 focus:ring-palette-cyan-300",
+        secondary:
+          "text-on-neutral-dark bg-palette-purple-400 hover:bg-palette-purple-500 focus:ring-palette-purple-400 dark:text-on-neutral-light dark:bg-palette-purple-500 dark:hover:bg-palette-purple-600 dark:focus:ring-palette-purple-500",
+        accent:
+          "text-on-neutral-dark bg-palette-yellow-400 hover:bg-palette-yellow-500 focus:ring-palette-yellow-400 dark:hover:text-on-neutral-light dark:bg-palette-yellow-500 dark:hover:bg-palette-yellow-600 dark:focus:ring-palette-yellow-500",
       },
       size: {
         sm: "px-4 py-2 text-sm gap-2",
         md: "px-6 py-3 text-lg gap-3",
         lg: "px-8 py-4 text-2xl gap-4",
       },
+      outline: {
+        true: ["bg-transparent", "border-2", "dark:bg-transparent"],
+      },
     },
     defaultVariants: {
       variant: "default",
       size: "md",
     },
+    compoundVariants: [
+      {
+        variant: "default",
+        outline: true,
+        class: [
+          // Light Theme
+          "border-surface-metal-dark",
+          "text-surface-metal-dark",
+          "hover:text-surface-secondary-light",
+          // Dark Theme
+          "dark:border-surface-metal-light",
+          "dark:text-surface-metal-light",
+          "dark:hover:text-surface-secondary-dark",
+        ],
+      },
+      {
+        variant: "primary",
+        outline: true,
+        class: [
+          "border-palette-cyan-300",
+          "text-palette-cyan-300",
+          "hover:text-on-primary-light",
+          "hover:bg-palette-cyan-300",
+          "dark:hover:bg-palette-cyan-300",
+        ],
+      },
+    ],
   },
 )
 
@@ -35,15 +68,19 @@ export interface ButtonProps
   extends React.HTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
+  outline?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ variant, size, className, asChild, ...props }: ButtonProps, ref) => {
+  (
+    { variant, outline, size, className, asChild, ...props }: ButtonProps,
+    ref,
+  ) => {
     const Component = asChild ? Slot : "button"
     return (
       <Component
         ref={ref}
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size, outline, className }))}
         {...props}
       />
     )
