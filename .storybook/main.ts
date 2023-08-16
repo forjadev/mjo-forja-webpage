980be299fa873e0bb26ae405c37ee1c98b5f4a3c
Arthur Andrade <git@arthur404.dev>
2023-08-16 15:10:46 -0300

    feat: [MJO-Storybook] - Setup StoryBook with Themes and Provide Standards (#18)
    * chore: clean storybook defaults
    
    * feat: update storybook and dependencies
    
    * chore: add logo variations
    
    * feat: add theme support with context for each variant
    
    * feat: add Button Story and remove Buttons from homepage
    
    * feat: update color definition on tailwindcss
    
    * feat: add color palette documentation
    
    * feat: update logo and highlight color of storybook
    
    * feat: update button component to use new color palette

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,23 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+import type { StorybookConfig } from "@storybook/nextjs"
+import path from "path"
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "storybook-dark-mode",
+    "storybook-addon-themes",
+    {
+      name: "@storybook/addon-styling",
+      options: {
+        postCss: {
+          implementation: require("postcss"),
+        },
+      },
+    },
   ],
   framework: {
     name: "@storybook/nextjs",
@@ -14,5 +26,14 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
-};
-export default config;
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@": path.resolve(__dirname, "../src"),
+      }
+    }
+    return config
+  },
+}
+export default config
