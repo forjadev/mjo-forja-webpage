daa55a02dc7085a2bd849b4e53d4a5d2960ed3f7
Arthur Andrade <git@arthur404.dev>
2023-08-16 01:53:28 -0300

    feat: add Button Story and remove Buttons from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,15 @@
 "use client"
 
-import { Button } from "@/components"
+import {} from "@/components"
 
 export default function Home() {
   return (
-    <main className='flex h-screen flex-col items-center justify-between p-24 bg-surface-primary'>
+    <main className="flex h-screen flex-col items-center justify-between p-24 bg-surface-primary">
       <div
-        className='flex flex-col items-center justify-center gap-36 w-3/4 h-2/3
+        className="flex flex-col items-center justify-center gap-36 w-3/4 h-2/3
       bg-surface-secondary
-      rounded-xl ring-1 ring-metal-dark'
-      >
-        <div className='flex gap-4'>
-          <Button size='sm' variant='callers' onClick={() => alert("Do Botão")}>
-            Botão Caller
-          </Button>
-          <Button size='md' variant='callers'>
-            Botão Caller
-          </Button>
-          <Button size='lg' variant='callers'>
-            Botão Caller
-          </Button>
-        </div>
-        <div className='flex gap-4'>
-          <Button size='sm' variant='info'>
-            Botão Info
-          </Button>
-          <Button size='md' variant='info'>
-            Botão Info
-          </Button>
-          <Button size='lg' variant='info'>
-            Botão Info
-          </Button>
-        </div>
-      </div>
+      rounded-xl ring-1 ring-metal-dark"
+      ></div>
     </main>
   )
 }
