d1c0ab4d19d483805370c91a39a358ae3e1c5350
DhemesonSiqueira <dhemeson_loran@hotmail.com>
2023-08-18 23:38:02 -0300

    feat: add responsive design to community card component

--- src/components/communityCard/CommunityCard.tsx
+++ src/components/communityCard/CommunityCard.tsx
@@ -9,16 +9,23 @@ interface CardProps {
 const CommunityCard: React.FC<CardProps> = ({ icon, title, paragraph }) => {
   return (
       <div
-        className='bg-surface-secondary-light dark:bg-surface-secondary-dark p-[32px] w-[364px] h-[314px] rounded-[8px] border border-palette-blue-500/40 dark:border-palette-green-800/50'
+        className='bg-surface-secondary-light p-5 w-11/12 max-w-[364px] max-h-[314px] rounded-[8px] border border-palette-blue-500/40 relative
+        dark:bg-surface-secondary-dark dark:border-palette-green-800/50
+         sm:p-[32px] sm:w-[364px] sm:h-[314px]'
       >
 
-        <Image src={icon} alt='image' width={64} height={64}/>
+        <Image src={icon} alt='image' width={64} height={64} className='absolute right-5 top-[-20px] sm:static'/>
 
-        <h1 className='mt-8 text-palette-blue-600 dark:text-palette-cyan-400 text-[32px] font-bold leading-8'>
+        <h1 className='text-palette-blue-600 text-2xl font-bold leading-4
+          dark:text-palette-cyan-400
+          sm:leading-8 sm:mt-8 sm:text-[32px]'
+        >
           {title}
         </h1>
 
-        <p className='mt-2 text-base text-on-secondary-light dark:text-on-primary-dark leading-4'>
+        <p className='mt-3 text-base text-on-secondary-light
+          dark:text-on-primary-dark leading-4'
+        >
           {paragraph}
         </p>
       </div>
