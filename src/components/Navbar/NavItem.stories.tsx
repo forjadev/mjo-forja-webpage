a8687a012d40752c0c80e5aac7e6760adfa01a2f
unknown <61201131+kevinCubas@users.noreply.github.com>
2023-08-29 17:04:51 -0300

    refactor(stories): create template and adapt the navitem args

--- src/components/Navbar/NavItem.stories.tsx
+++ src/components/Navbar/NavItem.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryFn } from "@storybook/react";
 import { NavItem } from "./NavItem";
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 
@@ -6,6 +6,7 @@ const meta = {
   component: NavItem,
   parameters: {
     layout: "centered",
+    controls: { exclude: ["asChild"] },
   },
   decorators: [
     (Story) => (
@@ -14,32 +15,54 @@ const meta = {
       </NavigationMenu.Root>
     ),
   ],
+  argTypes: {
+    isActive: {
+      type: 'boolean',
+      description: 'Whether the link is active or not',
+      table: {
+        defaultValue: { summary: false },
+        type: { summary: 'boolean' },
+      }
+    },
+    href: {
+      type: 'string',
+      description: 'The URL to link to ',
+      table: {
+        defaultValue: { summary: '/' },
+        type: { summary: 'string' },
+      },
+    },
+    children: {
+      type: 'string',
+      description: 'The text to display inside the link',
+    }
+  },
   tags: ["autodocs"],
 } satisfies Meta<typeof NavItem>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    textPrimary: "Default",
-    href: "/",
-  },
-};
+const Template: StoryFn<typeof NavItem> = ({href = "/", ...args}) => {
+  return <NavItem href={href} {...args} />;
+}
 
-export const Active: Story = {
-  args: {
-    textPrimary: "Active",
-    href: "/",
-    variant: "isActive",
-  },
-};
+export const Default = Template.bind({});
+Default.args = {
+  children: "Início",
+}
 
-export const ActiveWithTextSecondary: Story = {
-  args: {
-    textPrimary: "Active",
-    textSecondary: "Secondary",
-    href: "/",
-    variant: "isActive",
-  },
-};
+export const Active = Template.bind({});
+Active.args = {
+  ...Default.args,
+  isActive: true,
+}
+
+export const ActiveWithPRO = Template.bind({});
+ActiveWithPRO.args = {
+  isActive: true,
+  children: (
+    <>
+      Forja <span className='font-bold text-palette-orange-500 whitespace-pre'> PRO</span>
+    </>
+  ),
+}
