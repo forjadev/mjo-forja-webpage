83d9935e6c4629a5c3af9812b31c36f431d3be99
unknown <61201131+kevinCubas@users.noreply.github.com>
2023-08-29 20:41:28 -0300

    fix: remove unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-"use client"
-
-import { Button } from "@/components"
+import {} from "@/components"
 
 export default function Home() {
   return (
@@ -8,31 +6,8 @@ export default function Home() {
       <div
         className="flex flex-col items-center justify-center gap-36 w-3/4 h-2/3
       bg-surface-secondary
-      rounded-xl ring-1 ring-metal-dark"
-      >
-        <div className='flex gap-4'>
-          <Button size='sm' variant='callers' onClick={() => alert("Do Botão")}>
-            Botão Caller
-          </Button>
-          <Button size='md' variant='callers'>
-            Botão Caller
-          </Button>
-          <Button size='lg' variant='callers'>
-            Botão Caller
-          </Button>
-        </div>
-        <div className='flex gap-4'>
-          <Button size='sm' variant='info'>
-            Botão Info
-          </Button>
-          <Button size='md' variant='info'>
-            Botão Info
-          </Button>
-          <Button size='lg' variant='info'>
-            Botão Info
-          </Button>
-        </div>
-      </div>
+      rounded-xl ring-1 ring-surface-metal-dark"
+      ></div>
     </main>
   )
 }
